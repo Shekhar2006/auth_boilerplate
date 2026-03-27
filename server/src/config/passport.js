1fdbc2435de4206a0121df19f04d22756cc39216
Shekhar2006 <shekharphulsare@gmail.com>
2026-03-27 19:53:38 +0530

    Done with server side and fix some issues

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GitHubStrategy = require('passport-github2').Strategy;
